2a9c669e3ae7b38b2f3d2549de1ecc6b3ba477ba
Arindam <ariron123@gmail.com>
2022-11-24 18:21:39 +0530

    Data Table Added

--- components/CreateOffer/Products.js
+++ components/CreateOffer/Products.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import { Button, Grid, Typography } from "@mui/material";
 import OutlinedInput from "@mui/material/OutlinedInput";
+import { DataGrid } from "@mui/x-data-grid";
 
 function Products(props) {
   const [upc, setUpc] = React.useState("");
@@ -10,6 +11,94 @@ function Products(props) {
   const [psa, setPsa] = React.useState("");
   const [comm, setComm] = React.useState("");
   const [subComm, setSubComm] = React.useState("");
+
+  const columns = [
+    { field: "slin", headerName: "SLIN", width: 150 },
+    {
+      field: "upc",
+      headerName: "UPC",
+      width: 220,
+    },
+    {
+      field: "description",
+      headerName: "Description",
+      width: 300,
+      editable: true,
+    },
+    {
+      field: "psa",
+      headerName: "PSA",
+      type: "number",
+      width: 200,
+      editable: true,
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      slin: "303024",
+      upc: "0004520506591",
+      description: "Harshy Snack Mix Tube",
+      psa: 35,
+    },
+    {
+      id: 2,
+      slin: "128096",
+      upc: "0090100506591",
+      description: "Carolina Snack Ring",
+      psa: 10,
+    },
+    {
+      id: 3,
+      slin: "103084",
+      upc: "0041100506591",
+      description: "Skol LC Spearmint 1.2z",
+      psa: 12,
+    },
+    {
+      id: 4,
+      slin: "303024",
+      upc: "0025412506591",
+      description: "24/7 Life Laundry Detergent 5Oz",
+      psa: 16,
+    },
+    {
+      id: 5,
+      slin: "785458",
+      upc: "0880100506591",
+      description: "Glass Cleaner Set",
+      psa: 14,
+    },
+    {
+      id: 8,
+      slin: "303024",
+      upc: "0030100658745",
+      description: "Beer Cap XL",
+      psa: 15,
+    },
+    {
+      id: 9,
+      slin: "123012",
+      upc: "0060100506791",
+      description: "CRA Shirt Red 2XL",
+      psa: 20,
+    },
+    {
+      id: 10,
+      slin: "951745",
+      upc: "0050100508588",
+      description: "Lance Cinanamon Roll 4z",
+      psa: 14,
+    },
+    {
+      id: 11,
+      slin: "365123",
+      upc: "0060100506521",
+      description: "Kraft Sweet and Sour Sauce",
+      psa: 10,
+    },
+  ];
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -131,6 +220,33 @@ function Products(props) {
           </Grid>
         </Grid>
       </Grid>
+      <Box sx={{ height: 400, width: "100%", mt: 1 }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          ppsaSize={5}
+          rowsPerPpsaOptions={[5]}
+          checkboxSelection
+          disableSelectionOnClick
+        />
+      </Box>
+      <Box
+        sx={{
+          textAlign: "right",
+        }}
+      >
+        <Button
+          onClick={() => {}}
+          sx={{
+            m: 1,
+            border: 1,
+            borderColor: "primary.main",
+            borderRadius: 1,
+          }}
+        >
+          Add Selected Items for Promotion
+        </Button>
+      </Box>
     </Box>
   );
 }
