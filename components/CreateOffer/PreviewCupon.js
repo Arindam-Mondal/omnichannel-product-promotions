54804db3f23fd4fb6dc810208fa01c86388ec1fb
Arindam <ariron123@gmail.com>
2022-11-25 16:20:54 +0530

    Preview Cuopun added

--- components/CreateOffer/PreviewCupon.js
+++ components/CreateOffer/PreviewCupon.js
@@ -1,7 +1,35 @@
+import Image from "next/image";
 import Box from "@mui/material/Box";
+import { Grid, Typography } from "@mui/material";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import IconButton from "@mui/material/IconButton";
 
 function PreviewCupon(props) {
-  return <Box sx={{ display: "flex" }}>PreviewCupon</Box>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container rowSpacing={2}>
+        <Grid container item xs={5}>
+          <Grid item xs={12}></Grid>
+          <Grid item xs={12}>
+            <Typography component="span">Upload a different image </Typography>
+            <IconButton
+              color="primary"
+              aria-label="upload picture"
+              component="label"
+            >
+              <input hidden accept="image/*" type="file" />
+              <PhotoCamera />
+            </IconButton>
+          </Grid>
+          <Grid item xs={12}></Grid>
+        </Grid>
+        <Grid item xs={4}>
+          <Image src="/mobile_mokup.png" width={200} height={300} />
+        </Grid>
+        <Grid item xs={3}></Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default PreviewCupon;
--- pages/create-offer/index.js
+++ pages/create-offer/index.js
@@ -88,7 +88,7 @@ export default function Home() {
           {allStepsCompleted() ? (
             <React.Fragment>
               <Typography sx={{ mt: 2, mb: 1 }}>
-                All steps completed - you&apos;re finished
+                Cupon Created Successfully - you&apos;re finished
               </Typography>
               <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
                 <Box sx={{ flex: "1 1 auto" }} />
